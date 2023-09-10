5ed79f6133c3dda8144b743178c7b3305565033d
antipin-ia <antipinivan0491@gmail.com>
2023-09-11 02:57:40 +0300

    link2

--- src/Pages/git.js
+++ src/Pages/git.js
@@ -5,8 +5,8 @@ const Git = () => {
 return (
 <main>
 <h1 className="git-title">Ссылка на проект</h1>
-<a href="https://github.com/antipin-ia/for_tic" className="git-link">Ссылка на проект на GitHub</a>
-{/* Добавьте здесь дополнительный контент */}
+<a href="https://github.com/antipin-ia/for_tic" className="git-link" target="_blank" rel="noopener noreferrer">Ссылка на проект на GitHub</a>
+
 </main>
 );
 };
